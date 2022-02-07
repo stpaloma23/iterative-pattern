0060e1a87091d191e7f26dab960ac5d01c4c5f73
Paloma St.Clair <palomast.clair@10-21-235-70.dynapool.wireless.nyu.edu>
2022-02-06 20:36:01 -0500

    iterative design

---
+++ src/main.ts
@@ -0,0 +1,77 @@
+import * as PIXI from "pixi.js"
+
+const load = (app: PIXI.Application) => {
+  return new Promise<void>((resolve) => {
+      app.loader.add('assets/hello-world.png').load(() => {
+          resolve();
+      });
+  });
+};
+
+const main = async () => {
+  // Actual app
+  let app = new PIXI.Application(); //making a new pixijs application 
+
+  // Display application properly
+  document.body.style.margin = '0';
+  app.renderer.view.style.position = 'absolute';
+  app.renderer.view.style.display = 'block';
+
+  // View size = windows
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+
+  // Load assets
+  await load(app);
+
+  let container = new PIXI.Container();
+  app.stage.addChild(container) ;
+
+  // let colorGradient = [0x8651bd,0x7b49b7,0x6f41b2,0x6339ac,0x5631a6,0x492aa1,0x3a239b,0x271c96];
+  let colorGradient = [0x8651bd,0x7b49b7,0x6f41b2,0x6339ac,0x5631a6,0x492aa1,0x3a239b,0x271c96,0x3a239b,0x492aa1,0x5631a6,0x6339ac,0x6f41b2,0x7b49b7,0x8651bd];
+  let gradIndex = 0; 
+  let currWidth = screen.width;
+  let currHeight = screen.height;
+  let posiitonOne = 0;
+  let positionTwo = 0;
+
+
+  // adding first rectangle to the drawing
+  let draw = new PIXI.Graphics();
+  draw.beginFill(colorGradient[gradIndex]);
+  draw.drawRect(0,0,currWidth,currHeight);
+  app.stage.addChild(draw);
+  gradIndex++; 
+  let bool = true
+  while(bool){
+    if (currWidth < (screen.width/50) ){
+      bool = false
+      break
+    }
+    currHeight -=10;
+    currWidth -=10;
+    posiitonOne += 4;
+    positionTwo += 4;
+    let rect = new PIXI.Graphics();
+    rect.beginFill(colorGradient[gradIndex]);
+    rect.drawRect(posiitonOne,positionTwo,currWidth,currHeight);
+    app.stage.addChild(rect);
+    gradIndex ++;
+    if(gradIndex == colorGradient.length){
+      gradIndex = 0;
+    }
+  }
+  // Handle window resizing
+  window.addEventListener('resize', (_e) => {
+      app.renderer.resize(window.innerWidth, window.innerHeight);
+    //   sprite.x = window.innerWidth / 2 - sprite.width / 2;
+    //   sprite.y = window.innerHeight / 2 - sprite.height / 2;
+    container.x = window.innerWidth/2;
+    container.y = window.innerHeight/2;
+  });
+
+  document.body.appendChild(app.view);
+
+};
+
+main();
+
